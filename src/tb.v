a863b9e6a0d3a76aed6f70cb1184cc16f6459f1b
Mason Xiao <masonx@cmu.edu>
2022-12-13 21:52:35 -0500

    simple counter

--- src/tb.v
+++ src/tb.v
@@ -10,25 +10,20 @@ module tb (
     // testbench is controlled by test.py
     input clk,
     input rst,
-    output [6:0] segments
+    output [7:0] outputs
    );
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
     initial begin
-        $dumpfile ("tb.vcd");
-        $dumpvars (0, tb);
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
         #1;
     end
 
     // wire up the inputs and outputs
     wire [7:0] inputs = {6'b0, rst, clk};
-    wire [7:0] outputs;
-    assign segments = outputs[6:0];
 
     // instantiate the DUT
-    seven_segment_seconds #(.MAX_COUNT(100)) seven_segment_seconds(
-        .io_in  (inputs),
-        .io_out (outputs)
-        );
+    mmx_chip dut(.io_in (inputs), .io_out(outputs));
 
 endmodule
--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_chip(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -16,8 +16,7 @@ async def test_7seg(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    dut._log.info("check all segments")
     for i in range(10):
-        dut._log.info("check segment {}".format(i))
         await ClockCycles(dut.clk, 100)
-        assert int(dut.segments.value) == segments[i]
+        dut._log.info("{}".format(int(dut.outputs.value)))
+        assert int(dut.outputs.value) == ((i*100 + 99) % 128)
